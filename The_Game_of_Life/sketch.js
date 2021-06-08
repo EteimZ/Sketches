8f3e1306ea47c30cd7e13371b54f62b49a5dc635
Eteimz <8teims@gmail.com>
2021-06-09 00:14:11 +0100

    Adding Life

---
+++ The_Game_of_Life/sketch.js
@@ -0,0 +1,90 @@
+// The Game of Life
+// Original code by Daniel Shiffman
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let life; // Audio player
+let grid;
+let cols;
+let rows;
+let resolution = 10;
+
+function setup() {
+  createCanvas(1000, 600);
+  cols = width / resolution;
+  rows = height / resolution;
+	life = createAudio('Frank-Sinatra-Thats-Life.mp3');
+	
+	
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
+	life.position(50, 610);
+  life.size(300);
+   
+  // Show the audio controls
+  //life.autoplay(true);
+	life.showControls();
+}
+
+function draw() {
+  background(0);
+
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+				fill( random(255), random(255), random(255) );
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+
+  let next = make2DArray(cols, rows);
+
+  // Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+	
+  grid = next;
+	
+}
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
